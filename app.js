90d799b4add5463a19910d89d599cd4190f97078
milohill <116620023+grimhilt7@users.noreply.github.com>
2023-01-29 12:57:34 +0900

    Complete human mode game

--- app.js
+++ app.js
@@ -1,124 +1,249 @@
 const playerSelectScreen = document.querySelector('.player-select');
-const humanButton = document.querySelector('#human');
-const robotButton = document.querySelector('#robot');
+const humanSelectButton = document.querySelector('#human');
+const robotSelectButton = document.querySelector('#robot');
 const playerOneIndicator = document.querySelector('.player-1');
 const playerTwoIndicator = document.querySelector('.player-2');
-const winnerIndicator = document.querySelector('.winner');
+const resultIndicator = document.querySelector('.winner');
 const restartButton = document.querySelector('#restart');
 const mainButton = document.querySelector('#main');
 const tiles = document.querySelectorAll('.tile');
 
-// UI events handlers
-function slideTheScreen() {
-  playerSelectScreen.classList.toggle('slide');
-}
-
-// human mode
-humanButton.addEventListener('click', () => {
-  slideTheScreen();
-  playerOneIndicator.textContent = 'Player X';
-  playerTwoIndicator.textContent = 'Player O';
-  playerOneIndicator.classList.add('slide-up');
-});
-
-// robot mode
-robotButton.addEventListener('click', () => {
-  slideTheScreen();
-  playerOneIndicator.textContent = 'You';
-  playerTwoIndicator.textContent = 'Bot';
-  playerOneIndicator.classList.add('slide-up');
-});
-
-// restart
-restartButton.addEventListener('click', () => {
-  playerOneIndicator.classList.add('slide-up');
-});
-
-// back to the main menu
-mainButton.addEventListener('click', () => {
-  slideTheScreen();
-  document.querySelector('.slide-up').classList.remove('slide-up'); // confiscate the class from the element that has it
-});
-
 // player generator
-const Player = name => {
+const Player = (name, element) => {
   const playerName = name;
+  const indicator = element;
   const markers = [];
-  const addMarker = function(str) {
-    markers.push(str)
+  const addMarker = str => {
+    markers.push(str);
   }
-  return { playerName, markers, addMarker };
+  return { playerName, indicator, markers, addMarker };
 }
-const playerOne = Player('playerOne');
-const playerTwo = Player('playerTwo');
 
 // board controller
 const board = (() => ({
-  winingRows: [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']],
+  players: [],
+
+  winningRows: [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'], ['1', '5', '9'], ['3', '5', '7']],
 
   determineWinner(player) {
-    const rlt = this.winingRows.some(winningRow => winningRow.every(el => player.markers.includes(el))); // winner determining algorithm
+    const rlt = this.winningRows.some(winningRow => winningRow.every(el => player.markers.includes(el))); // winner determining algorithm
     if (rlt) {
       this.endTheGame();
     }
   },
 
-  endTheGame() {
-    document.querySelector('.slide-up').classList.remove('slide-up'); // confiscate the class from the element that has it
-    const winnerName = this.getCurrentPlayer().playerName === 'playerOne' ? playerOneIndicator.textContent : playerTwoIndicator.textContent;
-    winnerIndicator.textContent = `${winnerName} win!`;
-    winnerIndicator.classList.add('win');
+  drawTheGame() {
+    resultIndicator.textContent = 'Draw!';
+    resultIndicator.classList.add('visible'); // make the indicator visible
+
+    tiles.forEach(tile => {
+      tile.removeEventListener('click', enableClickEffect);
+      tile.removeEventListener('mouseover', enableMouseoverEffect);
+      tile.removeEventListener('mouseout', enableMouseoutEffect);
+      tile.style.cursor = 'default';
+    }) // make tiles non-clickable, set the cursor default
+
+    document.querySelector('.highlighted').classList.remove('highlighted');
+    // hide player indicators
   },
 
-  occupiedMarkers: [],
-  whoseTurn: true,
+  endTheGame() {
+    resultIndicator.textContent = `${this.getCurrentPlayer().indicator.textContent} win!`;
+    resultIndicator.classList.add('visible'); // make the indicator visible
+
+    tiles.forEach(tile => {
+      tile.removeEventListener('click', enableClickEffect);
+      tile.removeEventListener('mouseover', enableMouseoverEffect);
+      tile.removeEventListener('mouseout', enableMouseoutEffect);
+      tile.style.cursor = 'default';
+    }) // make tiles non-clickable, set the cursor default
+
+    document.querySelector('.highlighted').classList.remove('highlighted');
+    // hide player indicators
+  },
+
+  markers: [],
+  turn: true, // crucial to determine several functions within
 
   flipTurn() {
-      if (this.whoseTurn) {
-        playerOneIndicator.classList.remove('slide-up');
-        playerTwoIndicator.classList.add('slide-up');
-      } else {
-        playerOneIndicator.classList.add('slide-up');
-        playerTwoIndicator.classList.remove('slide-up');
-      }
-    this.whoseTurn = !this.whoseTurn;
+    this.getCurrentPlayer().indicator.classList.remove('highlighted');
+    this.whoseTurn = !this.turn;
+    this.getCurrentPlayer().indicator.classList.add('highlighted');
   },
 
   getCurrentPlayer() {
-    return this.whoseTurn ? playerOne : playerTwo;
+    return this.turn ? this.players[0] : this.players[1];
   },
 
-  getSign() { return this.whoseTurn ? 'x' : 'o' },
+  getWaitingPlayer() {
+    return !this.turn ? this.players[0] : this.players[1];
+  },
+
+  getSign() { return this.turn ? 'x' : 'o' },
 
   getTileNumber(object) { return object.classList[1].slice(-1)}
 }))();
 
-// --tile setup--
-// tile click effect
-tiles.forEach(tile => tile.addEventListener('click', function() {
+// initialize function
+function initialize() {
+  board.turn = Math.random() > 0.5; // randomly choose one of the players
+  board.getCurrentPlayer().indicator.classList.add('highlighted');
+  [board.players[0].markers, board.players[1].markers] = [[], []];
+  board.markers = [];
+  tiles.forEach(tile => {
+    tile.addEventListener('click', enableClickEffect);
+    tile.addEventListener('mouseover', enableMouseoverEffect);
+    tile.addEventListener('mouseout', enableMouseoutEffect);
+    tile.style.cursor = 'pointer';
+  })
+  resultIndicator.classList.remove('visible');
+}
+
+// UI event handlers
+// human mode
+humanSelectButton.addEventListener('click', () => {
+  playerSelectScreen.classList.toggle('slide'); // player select screen slide up animation
+
+  playerSelectScreen.addEventListener('transitionend', (event) => {
+    if (event.propertyName === 'transform') {
+      initialize();
+    }
+  });
+
+  const playerOne = Player('playerOne', playerOneIndicator);
+  const playerTwo = Player('playerTwo', playerTwoIndicator);
+  board.players = [playerOne, playerTwo];
+
+  playerOneIndicator.textContent = 'Player X';
+  playerTwoIndicator.textContent = 'Player O';
+});
+
+// robot mode
+robotSelectButton.addEventListener('click', () => {
+
+});
+
+// restart
+restartButton.addEventListener('click', () => {
+  initialize();
+  updateDisplay();
+})
+
+// back to the main menu
+mainButton.addEventListener('click', () => {
+
+});
+
+// tile setup
+
+function updateDisplay() {
+  tiles.forEach((tile, idx) => {
+    const factor = (idx+1).toString();
+
+    if (board.players[0].markers.includes(factor)) {
+      tile.style.backgroundImage = `url('./images/x-sign.svg')`;
+    } else if (board.players[1].markers.includes(factor)) {
+      tile.style.backgroundImage = `url('./images/o-sign.svg')`;
+    } else {
+      tile.style.backgroundImage = ``;
+    }
+  })
+}
+
+function enableClickEffect() {
   const number = board.getTileNumber(this);
+  if (!board.markers.includes(number)) {
+    board.markers.push(number);
+    board.getCurrentPlayer().markers.push(number);
 
-  board.occupiedMarkers.push(number);
-  board.getCurrentPlayer().markers.push(number);
-
-  this.style.backgroundImage = `url('./images/${board.getSign()}-sign.svg')`;
+    updateDisplay();
+    
+    board.determineWinner(board.getCurrentPlayer());
   
-  board.determineWinner(board.getCurrentPlayer());
-  
-  board.flipTurn();
-}));
+    board.flipTurn();
+  }
+};
 
-// tile hover effect
-tiles.forEach(tile => tile.addEventListener('mouseover', function() {
+function enableMouseoverEffect() {
   const number = board.getTileNumber(this);
-  if (!board.occupiedMarkers.includes(number)) {
+  if (!board.markers.includes(number)) {
     this.style.backgroundImage = `url('./images/${board.getSign()}-sign.svg')`;
   }
-}))
 
-tiles.forEach(tile => tile.addEventListener('mouseout', function() {
+}
+
+function enableMouseoutEffect() {
   const number = board.getTileNumber(this);
-  if (!board.occupiedMarkers.includes(number)) {
+  if (!board.markers.includes(number)) {
     this.style.backgroundImage = "";
   }
-}))
+}
+
+// --AI--
+function getPossibleWinningRows(player) {
+  const objOne = {};
+  const objTwo = {};
+ 
+  // player one's winning rows
+  board.players[0].markers.forEach(marker => {
+    board.winningRows.forEach(row => {
+      if (row.includes(marker)) {
+        objOne[row] = (objOne[row] || 0) + 1;
+      }
+    })
+  })
+
+  // player two's winning rows
+  board.players[1].markers.forEach(marker => {
+    board.winningRows.forEach(row => {
+      if (row.includes(marker)) {
+        objTwo[row] = (objTwo[row] || 0) + 1;
+      }
+    })
+  })
+
+  if (player === board.players[0]) {
+    return Object.keys(objOne).reduce((acc, cur) => !Object.keys(objTwo).includes(cur) ? { ...acc, [cur]: objOne[cur]} : acc, {});
+  } 
+    return Object.keys(objTwo).reduce((acc, cur) => !Object.keys(objOne).includes(cur) ? { ...acc, [cur]: objTwo[cur]} : acc, {});
+  // returns possible winning rows with priority given the markers that have been chosen
+};
+
+function tellIfImWinning(player) {
+  const tempOne = Math.max(...Object.values(getPossibleWinningRows(playerOne)));
+  const tempTwo = Math.max(...Object.values(getPossibleWinningRows(playerTwo)));
+
+  if (tempOne === tempTwo) {
+    return 0;
+  }
+
+  if (tempOne > tempTwo) {
+    return player === playerOne;
+  } 
+  
+  return player === playerTwo; // if tempTwo is bigger
+  
+} // returns true -> attack, false -> block, 0 -> attack and block
+
+function chooseTile(player) {
+  if (tellIfImWinning(player) === 0) {
+    attackAndBlock(player)
+  }
+
+  if (tellIfImWinning(player)) {
+    exert(player);
+  } else {
+    exert(board.returnTheOpponent(player));
+  }
+
+} // returns str 
+
+function attackAndBlock(arr) {
+
+  exert(player)
+
+}
+
+function exert(arr) {
+
+}
